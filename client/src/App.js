d0fd2fd7fbe40601999291f3aace1a023bfc599f
Gord L <rndedg@gmail.com>
2023-04-18 13:16:25 -0700

    Add functionality to Join Room Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import io from "socket.io-client";
+import Home from "./pages/home";
+
+const socket = io.connect("http://localhost:4000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
