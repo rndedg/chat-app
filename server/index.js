678a9e208f90ec26bd31b634bf3be43014a5c404
Gord L <rndedg@gmail.com>
2023-04-18 13:38:04 -0700

    Created server messages and event handlers for users joining rooms

--- server/index.js
+++ server/index.js
@@ -13,10 +13,39 @@ const io = new Server(server, {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   }
-})
+});
+
+const CHAT_BOT = 'ChatBot';
+let chatRoom = '';
+let allUsers = [];
 
 io.on('connection', (socket) => {
   console.log(`User connected ${socket.id}`)
+
+  socket.on('join_room', (data) => {
+    const { username, room } = data;
+    socket.join(room);
+
+    let __createdtime__ = Date.now();
+
+    socket.to(room).emit('receive_message', {
+      message: `${username} has entered the room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    socket.emit('receive_message', {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
+
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+    chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit('chatroom_users', chatRoomUsers);
+    socket.emit('chatroom_users', chatRoomUsers);
+  });
 });
 
 app.get('/', (req, res) => {
